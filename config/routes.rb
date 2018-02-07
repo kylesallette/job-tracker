905096ec1c8c27c9ca078a0e8e22b2211f358e75
kylesallette <kylesallette@gmail.com>
2018-02-06 23:16:17 -0700

    adding comment table

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
     resources :categories do
       resources :jobs
-  end 
+  end
+     root 'companies#dashboard'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
